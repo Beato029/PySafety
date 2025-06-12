a92b24aa56edf4a32eb03635b117a379623e93aa
Beato029 <kalibeato1983@gmail.com>
2025-06-12 18:45:50 +0200

    Update update.py

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import os
 
 os.system('sudo git clone https://github.com/Beato029/PySafety.git')
 os.system('sudo rm -rf main.py')
-os.system('sudo mv /PySafety/main.py .')
+path = os.getcwd()
+os.system(f'sudo mv ./PySafety/main.py {path}')
 os.system('sudo rm -rf PySafety')
 os.system('sudo python main.py')
