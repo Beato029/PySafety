59a7517ba4b99a92776ae08ccef38411b946b2e4
Beato029 <kalibeato1983@gmail.com>
2026-04-14 23:11:50 +0200

    Initial clean commit

---
+++ d
@@ -0,0 +1,83 @@
+[33mcommit d9ed27f337b4b33a6142ec762acf83140fe33647[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Tue Apr 14 18:48:38 2026 +0200
+
+    commit
+
+[33mcommit 126d290403310ed4739dcf21a8b3fef0febccfcb[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Tue Apr 14 16:21:40 2026 +0200
+
+    commit
+
+[33mcommit 9791361c3e38825ecfb7531e3fd8ad675590ac70[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 21:44:03 2026 +0200
+
+    commit
+
+[33mcommit aca959a6422cbe8dbb02f79f78bbb85426534d45[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 21:42:29 2026 +0200
+
+    commit
+
+[33mcommit c4410501af7b9d98b6bf9bae1a704b0fb4bc1758[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 21:39:49 2026 +0200
+
+    commit
+
+[33mcommit 2db3a761c510d7aa6abc372df3e427ba706dd41c[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 21:34:03 2026 +0200
+
+    commit
+
+[33mcommit 163c3d961134b962b886dce3d6ceb2e24f0b8df8[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 19:41:30 2026 +0200
+
+    commit
+
+[33mcommit d2fd217ec005d9b3f43d29ae063de4119319d88f[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 19:39:05 2026 +0200
+
+    commit
+
+[33mcommit d090e24e7e7ecc93d2fe81c070aea1c971e0c139[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Mon Apr 13 19:36:41 2026 +0200
+
+    commit
+
+[33mcommit 0be21cf11475c94b7ae2dfbe6049d3c61f2fca5d[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Sun Apr 12 23:48:46 2026 +0200
+
+    commit
+
+[33mcommit 8437230c7d60a891d6077349ae9a3b79a7110724[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Sun Apr 12 23:43:49 2026 +0200
+
+    commit
+
+[33mcommit c8ecbef29bb1eed47fc4a55a582857ba59cd9bca[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Sun Apr 12 23:41:24 2026 +0200
+
+    commit
+
+[33mcommit 08bb033ec0d862aab610d4320a7cf8817e2ed4b9[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Sun Apr 12 23:37:14 2026 +0200
+
+    commit
+
+[33mcommit 26cac0ed73eff77bf3bfd8b268e2cfd5147e20c9[m
+Author: Beato029 <kalibeato1983@gmail.com>
+Date:   Sun Apr 12 22:34:30 2026 +0200
+
+    commit
---
+++ installer/installer.py
@@ -0,0 +1,189 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+import sys
+
+# pip install nuitka
+# python -m nuitka --onefile --lto=yes --mingw64 tuo_script.py
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PySafety Installer")
+
+        screen = QApplication.primaryScreen()
+        screen_geometry = screen.geometry()
+
+        perc = 0.15
+        w = int(screen_geometry.width() * perc)
+        h = int(screen_geometry.height() * perc)
+
+        self.setMinimumSize(w, h)
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(10, 0, 10, 10)
+        # self.main_layout.setSpacing(15)
+
+        self.label = QLabel("Preparazione sistema...")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setStyleSheet("font-size: 14px; font-weight: bold;")
+
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 100)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setTextVisible(True)
+
+        self.progress_bar.setStyleSheet("""
+            QProgressBar {
+                border: none;
+                border-radius: 10px;
+                background-color: #2a2a2a;
+                height: 22px;
+                text-align: center;
+                color: white;
+            }
+            QProgressBar::chunk {
+                border-radius: 10px;
+                background-color: qlineargradient(
+                    x1:0, y1:0, x2:1, y2:0,
+                    stop:0 #00d2ff,
+                    stop:1 #3a7bd5
+                );
+            }
+        """)
+
+        self.main_layout.addStretch()
+        # self.main_layout.addWidget(self.progress_bar)
+        self.main_layout.addWidget(self.label)
+        self.main_layout.addStretch()
+
+        self.setLayout(self.main_layout)
+
+
+        # self.timer = QTimer()
+        # self.timer.timeout.connect(self.update_progress)
+
+        # self.value  = 0
+        
+        self.askYesUpdate()
+        # self.start_loading()
+
+    def clear_window(self):
+        while self.main_layout.count():
+            item = self.main_layout.takeAt(0)
+            if item.widget():
+                item.widget().deleteLater()
+            elif item.layout():
+                while item.layout().count():
+                    child = item.layout().takeAt(0)
+                    if child.widget():
+                        child.widget().deleteLater()
+
+
+    def askYesUpdate(self):
+        self.label.setText("Procedere con l'installazione?")
+
+        frame = QFrame()
+        frame.setStyleSheet("""
+            QFrame {
+                background-color: dark-gray;
+                border-radius: 20px;
+                font-weight: bold;
+                font-style: italic;
+            }
+        """)
+
+        vertical_layout = QVBoxLayout()
+
+        label1 = QLabel("Il software è ancora in fase di sviluppo,", frame)
+        label2 = QLabel("la versione disponibile è una beta ad uso esclusivo di test,")
+        label3 = QLabel("possono causarsi bug o crash anomali")
+
+        vertical_layout.addWidget(label1, alignment=Qt.AlignmentFlag.AlignCenter)
+        vertical_layout.addWidget(label2, alignment=Qt.AlignmentFlag.AlignCenter)
+        vertical_layout.addWidget(label3, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        frame.setLayout(vertical_layout)
+
+        horizontal_layout = QHBoxLayout()
+
+        yes_button = QPushButton("Installa")
+
+        no_button = QPushButton("Annulla")
+        no_button.clicked.connect(self.close)
+
+        horizontal_layout.addWidget(no_button)
+        horizontal_layout.addWidget(yes_button)
+
+        # self.main_layout.addWidget(label1, alignment=Qt.AlignmentFlag.AlignCenter)
+        # self.main_layout.addWidget(label2, alignment=Qt.AlignmentFlag.AlignCenter)
+        # self.main_layout.addWidget(label3, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(frame)
+        self.main_layout.addLayout(horizontal_layout)
+
+    def install(self):
+        import requests
+        import time
+
+        url = "https://raw.githubusercontent.com/Beato029/PySafety/main/pysafety/pysafety.py"
+        output_file = "pysafety.py"
+
+        response = requests.get(url, stream=True)
+
+        if response.status_code != 200:
+            from tkinter import messagebox
+            messagebox.showerror("Errore", f"Errore: {response.status_code}")
+            exit()
+
+        total_size = int(response.headers.get("content-length", 0))
+        downloaded = 0
+        chunk_size = 1024
+
+        start_time = time.time()
+
+        with open(output_file, "wb") as f:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    f.write(chunk)
+                    downloaded += len(chunk)
+
+                    elapsed = time.time() - start_time
+                    speed = downloaded / elapsed if elapsed > 0 else 0
+
+                    eta = (total_size - downloaded) / speed if speed > 0 else 0
+                    percent = (downloaded / total_size) * 100 if total_size else 0
+
+                    sys.stdout.write(
+                        f"\r[{percent:.2f}%] "
+                        f"{downloaded/1024:.1f}KB / {total_size/1024:.1f}KB | "
+                        f"{speed/1024:.1f} KB/s | "
+                        f"ETA: {eta:.1f}s"
+                    )
+                    sys.stdout.flush()
+
+    def start_loading(self):
+        self.value = 0
+        self.progress_bar.setValue(self.value)
+        self.label.setText("Caricamento in corso...")
+        self.timer.start(20)
+
+    def update_progress(self):
+        if self.value >= 100:
+            self.timer.stop()
+            self.label.setText("Caricamento completato ✔")
+            return
+
+        # smooth increment
+        self.value += 0.8
+        self.progress_bar.setValue(int(self.value))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    app.setStyle("Fusion")
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
